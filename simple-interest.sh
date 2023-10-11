316297b5c9177ba49386a83e8a71c4878076f756
pthryambika <147626251+pthryambika@users.noreply.github.com>
2023-10-11 19:51:54 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,12 @@
+# Simple Interest Calculator
 
+# Input principal amount, rate of interest, and time period from the user
+principal = float(input("Enter the principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the time period (in years): "))
+
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Display the result
+print(f"The simple interest earned is: ${simple_interest:.2f}")
